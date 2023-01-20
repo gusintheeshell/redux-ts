7b7ca3281fea424a5f63c3fc8f2cee40a7dedd0f
Gustavo Barbosa <gusbdev@gmail.com>
2023-01-20 11:50:59 -0300

    feature: initial repository list

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,12 @@
+const RepositoriesList = (): JSX.Element => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
